12005b81c06e5ebd9775ea8bf7d5ae473c397241
kangtr <rkdaksquf@naver.com>
2024-08-06 10:36:55 +0900

    00변수 공부

---
+++ __00변수/class01.java
@@ -0,0 +1,19 @@
+package __00변수;
+
+public class class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 정수 변수 선언
+		int a =10;
+		byte aa =10;
+		System.out.println(a);
+		System.out.println(aa);
+		// 소스 실행하는 단축키 : Ctrl + f11
+		
+		// 문자열 변수 선언
+		String s ="Kangtr";
+		System.out.println(s);
+	}
+
+}
