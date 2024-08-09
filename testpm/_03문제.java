48ce7ef08072ed1c1d778f5534ac3661e1f80cb6
kangtr <rkdaksquf@naver.com>
2024-08-09 13:05:07 +0900

    _04문제

---
+++ testpm/_03문제.java
@@ -0,0 +1,13 @@
+package testpm;
+
+public class _03문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+	}
+
+}
---
+++ testpm/_04문제.java
@@ -0,0 +1,66 @@
+package testpm;
+
+public class _04문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 문제 1
+		
+		for(int i = 100; i < 1000; i++) {
+			if(i %2==1) {
+		
+				System.out.print(i+",");
+			
+			}
+		}	
+				System.out.println(" ");
+		// 문제 2
+		int sum = 0;
+		for(int i = 1; i < 100; i++) {
+			if(i %2==1) {
+				sum += i;
+			}
+			
+		}
+					System.out.println(sum);
+		
+		// 문제 3
+		
+		for(int i = 10; i > 0; i--) {
+			
+			
+			System.out.print(i+",");
+		}
+			System.out.println(" ");
+			
+		// 문제 4
+		// int[] arr = {45,23,25,64,3,24,48}; // {}안의 숫자:value(값) 
+		// [] : index 인덱스, arr[인덱스숫자] = value
+		int[] arr = {45,23,25,64,3,24,48};
+		for(int i = 0; i < 7; i++) {
+
+			if(arr[i]%2 == 0) {
+				System.out.print(arr[i]+",");
+			}
+		}
+				System.out.println(" ");
+				
+		// 문제 6
+		
+		for(int i = 0; i < 7; i++) {
+			if(arr[i]%2== 1) {
+				System.out.print(i+",");
+			}
+		}
+		// 문제 7
+		
+		for(int i = 0; i < 7; i++) {
+		
+			System.out.println(i);
+		}
+		// 문제 8
+		
+		
+	}
+
+}
