f31b1267f4900d6a1aedf38cc9e1465d2afcf2ce
kangtr <rkdaksquf@naver.com>
2024-08-13 17:29:57 +0900

    _04test

---
+++ for_final/_05test.java
@@ -0,0 +1,10 @@
+package for_final;
+
+public class _05test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ for_final/_06test.java
@@ -0,0 +1,11 @@
+package for_final;
+
+public class _06test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a ="23-56+45*2-56";
+		
+	}
+
+}
