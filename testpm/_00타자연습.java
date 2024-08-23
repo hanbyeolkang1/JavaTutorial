39a1980c8fabda11c828e3883904cc61962cf4c9
kangtr <rkdaksquf@naver.com>
2024-08-23 10:38:05 +0900

    _00타자연습

--- testpm/_00타자연습.java
+++ testpm/_00타자연습.java
@@ -8,22 +8,33 @@ public class _00타자연습 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] a = {"diamond","clover","space","heart"};
-		String[] b = {"다이아몬드","클로버","스페이스","하트"};
+		String[] a = {"diamond","clover","space","heart","","royalflysh","straightflush","fourofakind","fullhouse"};
+		String[] b = {"다이아몬드","클로버","스페이스","하트","로얄플러쉬","스트레이트플러쉬","포카드","풀하우스"};
 		Scanner in = new Scanner(System.in);
 		Random r = new Random();
-		int aToType = r.nextInt (a.length -1);  // 이렇게 하면 a 라는 배열에 4가지중 하나를 가져온다
+		int score = 0;
 		
 		
-			System.out.println("타자 게임에 오신것을 환영합니다.");
+			
+		for(int i = 0; i < 10; i++) {
+			int aToType = r.nextInt(a.length);  // 이렇게 하면 a 라는 배열에 4가지중 하나를 가져온다
 			System.out.println("다음 단어를 입력하세요.");
-		for(int i = 0; i < a.length; i++) {
-			if(a ==b) {
-				System.out.println("정답입니다"+"보너스"+a[i]);
+			System.out.println(a[aToType]);
+			String inputb = in.next();
+			if(b[aToType].equals(inputb)) {
+					score += 20;
+					
+				System.out.println("정답입니다");
 			}else {
 				System.out.println("틀렸습니다. 정답은:");
 			}
+				System.out.println("현재 점수는"+score+"입니다");
 		}
+			if(score == 100) {
+				System.out.println("100점 달성");
+			}else {
+				System.out.println("최정점수"+score+"점");
+			}
 	}
 
 }
